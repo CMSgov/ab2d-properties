bb98d99191ed1e8a6f5034804158a5bab2e978d0
Rwolfe-Nava <richardwolfe@navapbc.com>
2023-09-06 15:17:57 -0400

    initial commit for spring 3 upgrade

--- src/main/java/gov/cms/ab2d/properties/controller/HealthCheckController.java
+++ src/main/java/gov/cms/ab2d/properties/controller/HealthCheckController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.net.MalformedURLException;
--- src/main/java/gov/cms/ab2d/properties/model/Property.java
+++ src/main/java/gov/cms/ab2d/properties/model/Property.java
@@ -5,12 +5,12 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
 
