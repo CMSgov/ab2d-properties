65f49ec2a370a0055830f0751488b416b0d48aef
smirnovaae <annasmirnova@navapbc.com>
2023-09-26 09:59:10 -0700

    wip

--- src/main/resources/db/changelog/v2023/create_properties.sql
+++ src/main/resources/db/changelog/v2023/create_properties.sql
@@ -1,20 +1,20 @@
-CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE IF NOT EXISTS property.hibernate_sequence START WITH 1 INCREMENT BY 1;
 
 INSERT INTO property.properties (id, key, value)
-VALUES ((select nextval('hibernate_sequence')), 'hpms.ingest.engaged', 'engaged'),
-       ((select nextval('hibernate_sequence')), 'pcp.core.pool.size', 10),
-       ((select nextval('hibernate_sequence')), 'pcp.max.pool.size', 150),
-       ((select nextval('hibernate_sequence')), 'pcp.scaleToMax.time', 900),
-       ((select nextval('hibernate_sequence')), 'maintenance.mode', 'false'),
-       ((select nextval('hibernate_sequence')), 'worker.engaged', 'engaged'),
-       ((select nextval('hibernate_sequence')), 'coverage.update.discovery', 'idle'),
-       ((select nextval('hibernate_sequence')), 'coverage.update.queueing', 'idle'),
-       ((select nextval('hibernate_sequence')), 'ZipSupportOn', 'false');
+VALUES ((select nextval('property.hibernate_sequence')), 'hpms.ingest.engaged', 'engaged'),
+       ((select nextval('property.hibernate_sequence')), 'pcp.core.pool.size', 10),
+       ((select nextval('property.hibernate_sequence')), 'pcp.max.pool.size', 150),
+       ((select nextval('property.hibernate_sequence')), 'pcp.scaleToMax.time', 900),
+       ((select nextval('property.hibernate_sequence')), 'maintenance.mode', 'false'),
+       ((select nextval('property.hibernate_sequence')), 'worker.engaged', 'engaged'),
+       ((select nextval('property.hibernate_sequence')), 'coverage.update.discovery', 'idle'),
+       ((select nextval('property.hibernate_sequence')), 'coverage.update.queueing', 'idle'),
+       ((select nextval('property.hibernate_sequence')), 'ZipSupportOn', 'false');
 
 INSERT INTO property.properties(id, key, value, created, modified)
-VALUES (nextval('hibernate_sequence'), 'coverage.update.override', 'false', current_timestamp, current_timestamp),
-       (nextval('hibernate_sequence'), 'coverage.update.months.past', '3', current_timestamp, current_timestamp),
-       (nextval('hibernate_sequence'), 'coverage.update.stuck.hours', '24', current_timestamp, current_timestamp);
+VALUES (nextval('property.hibernate_sequence'), 'coverage.update.override', 'false', current_timestamp, current_timestamp),
+       (nextval('property.hibernate_sequence'), 'coverage.update.months.past', '3', current_timestamp, current_timestamp),
+       (nextval('property.hibernate_sequence'), 'coverage.update.stuck.hours', '24', current_timestamp, current_timestamp);
 
 SELECT setval('property.property_sequence', (SELECT max(id) FROM property.properties)+1);
 
