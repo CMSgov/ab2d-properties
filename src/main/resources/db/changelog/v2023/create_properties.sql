a6b5299c212489528dcd3590034452b9eb90165b
Sadibhatla <121067659+Sadibhatla@users.noreply.github.com>
2024-01-31 11:29:08 -0800

    Update create_properties.sql

--- src/main/resources/db/changelog/v2023/create_properties.sql
+++ src/main/resources/db/changelog/v2023/create_properties.sql
@@ -26,11 +26,11 @@ ON CONFLICT (key) DO UPDATE SET value = 'engaged';
 
 INSERT INTO property.properties (id, key, value)
 VALUES ((select nextval('property.hibernate_sequence')), 'coverage.update.discovery', 'engaged')
-ON CONFLICT (key) DO UPDATE SET value = 'idle';
+ON CONFLICT (key) DO UPDATE SET value = 'engaged';
 
 INSERT INTO property.properties (id, key, value)
 VALUES ((select nextval('property.hibernate_sequence')), 'coverage.update.queueing', 'engaged')
-ON CONFLICT (key) DO UPDATE SET value = 'idle';
+ON CONFLICT (key) DO UPDATE SET value = 'engaged';
 
 INSERT INTO property.properties (id, key, value)
 VALUES ((select nextval('property.hibernate_sequence')), 'ZipSupportOn', false)
